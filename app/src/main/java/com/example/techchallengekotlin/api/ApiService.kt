e760b3d15cc5ecb481d26d641272a8a1a6dc9323
KaduFloresta <94453200g>
2023-08-20 00:59:53 -0300

    Final Commit - edit Mode, edit Dialog, Design, Toolbar Logo and some fixes.

--- app/src/main/java/com/example/techchallengekotlin/api/ApiService.kt
+++ app/src/main/java/com/example/techchallengekotlin/api/ApiService.kt
@@ -1,10 +1,17 @@
 package com.example.techchallengekotlin.api
 
+import com.example.techchallengekotlin.data.UserData
 import com.example.techchallengekotlin.data.UserResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("ce47ee53-6531-4821-a6f6-71a188eaaee0")
     suspend fun getUsers(): Response<UserResponse>
+
+    @PUT("users/{id}")
+    suspend fun updateUser(@Path("id") userId: Int, @Body userData: UserData): Response<UserResponse>
 }
